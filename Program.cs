4639bd94296a4a1cf6569fbff3cc4818524986fc
Celso Jacinto de Siqueira Neto <120414414+CelsoSiqueira1996@users.noreply.github.com>
2022-12-21 02:41:22 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,67 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-class Questao02
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+
+class Questao03
 {
     static void Main()
     {
         string caminhoDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         string caminhoMatrizDistancias = Path.Combine(caminhoDesktop, "matriz.txt");
-        string[]? arrayDistancias = Arquivo.CarregarArquivo(caminhoMatrizDistancias);
         string caminhoPercurso = Path.Combine(caminhoDesktop, "caminho.txt");
-        string[]? percursoInformado = Arquivo.CarregarArquivo(caminhoPercurso);
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture){HasHeaderRecord = false};
+        using var readerDistancias = new StreamReader(caminhoMatrizDistancias) ;
+        using var csvDistancias = new CsvReader(readerDistancias, config);
+        using var readerCaminho = new StreamReader(caminhoPercurso);
+        using var csvCaminho = new CsvReader(readerCaminho, config);
+        int contadorDistancias = 0;
+        int contadorCaminho = 0;
+        int dimensaoMatriz = 0;
+        int dimensaoCaminho = 0;
         int distancia = 0;
+        int i = 0;
+        csvDistancias.Read();
+        csvCaminho.Read();
 
-        if (arrayDistancias == null || percursoInformado == null)
+        while (contadorDistancias != 1)
         {
-            Console.WriteLine("Matriz e/ou caminho não encontrados! Crie o arquivo matriz.txt e caminho.txt no desktop.");
-            return;
+            try
+            {
+                csvDistancias.GetField<int>(dimensaoMatriz);
+                dimensaoMatriz++;
+            }
+            catch
+            {
+                contadorDistancias++;
+            }
         }
 
-        int[][] distancias = new int[arrayDistancias.Length][];
-        int[] percurso = percursoInformado[0].Split(',').ToInt();
-        int contador = 0;
+        int[,] distancias = new int[dimensaoMatriz, dimensaoMatriz];
 
-        foreach (string line in arrayDistancias)
+        do
         {
-            distancias[contador] = line.Split(',').ToInt();
-            contador++;
+            for (int j = 0; j < dimensaoMatriz; j++)
+            {
+                distancias[i, j] = csvDistancias.GetField<int>(j);
+            }
+            i++;
+        }while(csvDistancias.Read());
+
+        while (contadorCaminho != 1)
+        {
+            try
+            {
+                csvCaminho.GetField<int>(dimensaoCaminho);
+                dimensaoCaminho++;
+            }
+            catch
+            {
+                contadorCaminho++;
+            }
         }
-        for (int i = 1; i < percurso.Length; i++)
+
+        int[] percurso = new int[dimensaoCaminho];
+
+        do
         {
-            distancia += distancias[percurso[i - 1] - 1][percurso[i] - 1];
+            for (int j = 0; j < dimensaoCaminho; j++)
+            {
+                percurso[j] = csvCaminho.GetField<int>(j);
+            }
+            i++;
+        } while (csvCaminho.Read());
+
+        for (int j = 1; j < percurso.Length; j++)
+        {
+            distancia += distancias[percurso[j - 1] - 1, percurso[j] - 1];
         }
         Console.WriteLine($"A distância total percorrida foi de {distancia} km.");
-    }
-}
-public static class Arquivo
-{
-    public static string[]? CarregarArquivo(string caminho)
-    {
-        try
-        {
-            return File.ReadAllLines(caminho);
-        }
-        catch
-        {
-            return null;
-        }
-    }
-}
-
-
-public static class Utils
-{
-    public static int[] ToInt(this string[] strings)
-    {
-        int[] val = new int[strings.Length];
-        for (int i = 0; i < strings.Length; i++)
-        {
-            int.TryParse(strings[i], out val[i]);
-        }
-        return val;
     }
 }
